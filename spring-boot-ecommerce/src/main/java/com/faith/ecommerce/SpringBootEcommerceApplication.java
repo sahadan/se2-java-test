4760624851b24a818d8995dcf401e06c2678d94c
sahadan <sahadan@gmail.com>
2020-09-15 16:33:29 +0530

    Third Commit

--- spring-boot-ecommerce/src/main/java/com/faith/ecommerce/SpringBootEcommerceApplication.java
+++ spring-boot-ecommerce/src/main/java/com/faith/ecommerce/SpringBootEcommerceApplication.java
@@ -8,7 +8,7 @@ public class SpringBootEcommerceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootEcommerceApplication.class, args);
-		System.out.println("Ecommerce App is running...Cool");
+		System.out.println("Ecommerce App is running...Cool App");
 	}
 
 }
